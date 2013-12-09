1601401548f40627de450c9926ffcccb8a2088cd
pubal <pubal@hotmail.com>
2013-12-09 01:12:32 -0500

    v 0.1.1
    Added IBM ISS Alertcon

--- src/com/intellavis/internetsecuritymonitor/MainActivity.java
+++ src/com/intellavis/internetsecuritymonitor/MainActivity.java
@@ -23,15 +23,21 @@ public class MainActivity extends Activity {
 	
 	public static final String SANS_infocon_url = 
 			"http://isc.sans.edu/images/status.gif";
-	ImageView imgView;
+	public static final String IBM_alertcon_url = 
+			"http://www-03.ibm.com/security/iss/img/alertcon.gif";
+	ImageView imgViewSANS;
+	ImageView imgViewIBM;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		imgView = (ImageView)findViewById(R.id.SANSinfocon);
-		ImageLoadTask task = new ImageLoadTask();
-		task.execute(new String[] { SANS_infocon_url });
+		imgViewSANS = (ImageView)findViewById(R.id.SANSinfocon);
+		ImageLoadTaskSANS taskSANS = new ImageLoadTaskSANS();
+		taskSANS.execute(new String[] { SANS_infocon_url });
+		ImageLoadTaskIBM taskIBM = new ImageLoadTaskIBM();
+		imgViewIBM = (ImageView)findViewById(R.id.IBMalertcon);
+		taskIBM.execute(new String[] { IBM_alertcon_url });
 	}	
 	
 	@Override
@@ -48,7 +54,14 @@ public class MainActivity extends Activity {
 		startActivity(infocon);
 	}
 	
-	private class ImageLoadTask extends AsyncTask<String, Void, Bitmap>{
+	public void gotoAlertcon(View view){
+		String alertURL = "http://securityintelligence.com/";
+		Intent alertcon = new Intent(Intent.ACTION_VIEW);
+		alertcon.setData(Uri.parse(alertURL));
+		startActivity(alertcon);
+	}
+	
+	private class ImageLoadTaskSANS extends AsyncTask<String, Void, Bitmap>{
 		
 		protected Bitmap doInBackground(String... url){
 			Bitmap bmp = null;
@@ -58,7 +71,40 @@ public class MainActivity extends Activity {
 		}
 		
 		protected void onPostExecute(Bitmap result){
-			imgView.setImageBitmap(result);
+			imgViewSANS.setImageBitmap(result);
+		}
+		
+		private Bitmap getBitmap(String url)
+		{
+			try
+			{
+			Bitmap bmp = null;
+			HttpClient client = new DefaultHttpClient();
+			URI imageURI = new URI(url);
+			HttpGet req = new HttpGet();
+			req.setURI(imageURI);
+			HttpResponse response = client.execute(req);
+			bmp = BitmapFactory.decodeStream(response.getEntity().getContent());
+			return bmp;
+			}catch (Exception e){
+				System.out.println("Exc="+e);
+				return null;
+			}
+		}
+		
+	}
+	
+	private class ImageLoadTaskIBM extends AsyncTask<String, Void, Bitmap>{
+		
+		protected Bitmap doInBackground(String... url){
+			Bitmap bmp = null;
+			bmp = getBitmap(url[0]);
+			
+			return bmp; 
+		}
+		
+		protected void onPostExecute(Bitmap result){
+			imgViewIBM.setImageBitmap(result);
 		}
 		
 		private Bitmap getBitmap(String url)
